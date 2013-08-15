56e80591ebb1fbbd3385426ab777cda12a78204c
fredhub <winkler@rzpc127.informatik.uni-hamburg.de>
2013-08-15 14:35:42 +0200

    Spielereien mit der for-Schleife, Lambdas & co.

--- spielwiese/Program.cs
+++ spielwiese/Program.cs
@@ -10,6 +10,18 @@ namespace spielwiese
     {
         static void Main(string[] args)
         {
+            foreach (var s in args)
+            {
+                Console.WriteLine(s + ": " + s.Length);
+            }
+            Console.WriteLine("---");
+
+            Array.ForEach(args, Console.WriteLine);
+            Console.WriteLine("---");
+            args.ToList().ForEach((s) => Console.WriteLine(s));
+            var sinnlos = from s in args select s;
+            Console.WriteLine(string.Join(",", args));
+
         }
     }
 }
